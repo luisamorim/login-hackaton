bda7c8495007d5e5e20f3402bd5cad06f95a8988
ademilson carvalho <ade.carvalho@outlook.com>
2020-02-27 17:26:17 -0300

    update user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user->update(
+            ['name' => $request['name'],
+             'password' => bcrypt($request['password'])
+        ]);
+
+        return redirect()->route('home');
+    }
+
+}
