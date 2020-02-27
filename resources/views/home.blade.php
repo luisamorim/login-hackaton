362c5a8480d2d23ecadbd2953dcae8217db76068
ademilson carvalho <ade.carvalho@outlook.com>
2020-02-27 16:34:00 -0300

    inclusão tela de edição

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,94 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">Edição de Usuário</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                                        <div class="panel-body">
+                                            <form class="form-horizontal" method="POST"
+                                                  action="{{ route('register') }}">
+                                                {{ csrf_field() }}
+
+                                                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                                    <label for="name" class="col-md-4 control-label">Name</label>
+
+                                                    <div class="col-md-6">
+                                                        <input id="name" type="text" class="form-control" name="name"
+                                                               value="{{ Auth::user()->name }}" required autofocus>
+                                                        @if ($errors->has('name'))
+                                                            <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                                        @endif
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                                    <label for="actualpassword"
+                                                           class="col-md-4 control-label">Actual Password</label>
+
+                                                    <div class="col-md-6">
+                                                        <input id="actualpassword" type="password" class="form-control"
+                                                               name="actualpassword" required>
+
+                                                        @if ($errors->has('password'))
+                                                            <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                                        @endif
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                                    <label for="password"
+                                                           class="col-md-4 control-label">Password</label>
+
+                                                    <div class="col-md-6">
+                                                        <input id="password" type="password" class="form-control"
+                                                               name="password" required>
+
+                                                        @if ($errors->has('password'))
+                                                            <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                                        @endif
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="password-confirm" class="col-md-4 control-label">Confirm
+                                                        Password</label>
+
+                                                    <div class="col-md-6">
+                                                        <input id="password-confirm" type="password"
+                                                               class="form-control" name="password_confirmation"
+                                                               required>
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <div class="col-md-6 col-md-offset-4">
+                                                        <button type="submit" class="btn btn-primary">
+                                                            Update
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    @endif
 
-                    You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
